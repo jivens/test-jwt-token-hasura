5ecde7209aebc6ab2a3761cd48adec05409e4fe8
amyfou <a.v.fountain@gmail.com>
2020-05-19 13:59:47 -0700

    updated auth

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
  module.exports = {
     port: process.env.PORT || 5001,
     facebookClientId: process.env.facebookClientId || 0000000000,
-    facebookClientSecret: process.env.facebookClientSecret || "",
-    privateTokenForJWT: "CIyPP8RBct8lPIqo1VCzwl3iX3qa0yV0",
+    facebookClientSecret: process.env.facebookClientSecret || "", 
+    privateTokenForJWT: "hzUU9Yt4yDFOmQVw0SoHuWmyeYnB1mJO",
     graphqlUrl: process.env.graphqlUrl || '',
+    expiresIn: "1d"
 };
--- create-jwt-token-factory.js
+++ create-jwt-token-factory.js
@@ -1,32 +1,45 @@
 const jwt = require("jsonwebtoken");
 const config = require("./config");
 
-const role = "user";
+const userRole = "user";
+const adminRole = "admin";
 
-const createJwtToken = body => jwt.sign(body, config.privateTokenForJWT);
+const createJwtToken = (body, expiresIn) => jwt.sign(body, config.privateTokenForJWT, expiresIn);
 
 exports.createAnonymousJwtToken = () =>
   createJwtToken({
     "https://hasura.io/jwt/claims": {
-      "x-hasura-allowed-roles": [role],
-      "x-hasura-default-role": role
+      "x-hasura-allowed-roles": [userRole],
+      "x-hasura-default-role": userRole
     }
-  });
+  }, 
+    { 
+      expiresIn: config.expiresIn
+    }
+  );
 
 exports.createGuestJwtToken = userId =>
   createJwtToken({
     "https://hasura.io/jwt/claims": {
-      "x-hasura-allowed-roles": [role],
-      "x-hasura-default-role": role,
+      "x-hasura-allowed-roles": [userRole],
+      "x-hasura-default-role": userRole,
       "x-hasura-user-id": userId
     }
-  });
+  }, 
+    { 
+      expiresIn: config.expiresIn
+    }
+  );
 
 exports.createAdminJwtToken = userId =>
   createJwtToken({
     "https://hasura.io/jwt/claims": {
-      "x-hasura-allowed-roles": [role],
-      "x-hasura-default-role": role,
+      "x-hasura-allowed-roles": [userRole, adminRole],
+      "x-hasura-default-role": adminRole,
       "x-hasura-user-id": userId
     }
-  });
+  }, 
+    { 
+      expiresIn: config.expiresIn
+    } 
+  );
